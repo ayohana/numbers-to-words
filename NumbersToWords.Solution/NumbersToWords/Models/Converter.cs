eead45d081a0b7651e9ad065d084670967a02eae
Sarah Schwartz <seschwartz8@gmail.com>
2020-03-05 11:40:43 -0800

    convert single digit numbers

--- NumbersToWords.Solution/NumbersToWords/Models/Converter.cs
+++ NumbersToWords.Solution/NumbersToWords/Models/Converter.cs
@@ -6,20 +6,20 @@ namespace NumbersToWords.Models
   public class Converter
   {
     public string Numeric { get; set; }
-    public Dictionary<string, string> Ones { get; set; }
-    public Dictionary<string, string> Teens { get; set; }
-    public Dictionary<string, string> Tens { get; set; }
+    private Dictionary<string, string> _ones;
+    private Dictionary<string, string> _teens;
+    private Dictionary<string, string> _tens;
 
     public Converter(string numeric)
     {
       Numeric = numeric;
-      Ones = new Dictionary<string, string>()
+      _ones = new Dictionary<string, string>()
       { { "1", "one" }, { "2", "two" }, { "3", "three" }, { "4", "four" }, { "5", "five" }, { "6", "six" }, { "7", "seven" }, { "8", "eight" }, { "9", "nine" }
       };
-      Teens = new Dictionary<string, string>()
+      _teens = new Dictionary<string, string>()
       { { "10", "ten" }, { "11", "eleven" }, { "12", "twelve" }, { "13", "thirteen" }, { "14", "fourteen" }, { "15", "fifteen" }, { "16", "sixteen" }, { "17", "seventeen" }, { "18", "eighteen" }, { "19", "nineteen" },
       };
-      Tens = new Dictionary<string, string>()
+      _tens = new Dictionary<string, string>()
       { { "2", "twenty" }, { "3", "thirty" }, { "4", "fourty" }, { "5", "fifty" }, { "6", "sixty" }, { "7", "seventy" }, { "8", "eighty" }, { "9", "ninety" },
       };
     }
@@ -28,15 +28,20 @@ namespace NumbersToWords.Models
     {
       string[] numArray = Numeric.Split("");
       int num;
+      List<string> output = new List<string>();
       if (!int.TryParse(Numeric, out num))
       {
         return "Invalid Input";
       }
       for (int i = numArray.Length - 1; i >= 0; i--)
       {
-
+        if (i == (numArray.Length - 1))
+        {
+          output.Insert(0, _ones[numArray[i]]);
+        }
       }
-      return "hi ";
+      string outputString = String.Join(" ", output.ToArray());
+      return outputString;
     }
   }
 }
