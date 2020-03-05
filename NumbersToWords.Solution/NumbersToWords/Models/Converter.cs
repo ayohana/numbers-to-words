7e0d282e2b875476adef44abf6619651c832abbc
Sarah Schwartz <seschwartz8@gmail.com>
2020-03-05 11:06:06 -0800

    create dictionaries for digit conversion

--- NumbersToWords.Solution/NumbersToWords/Models/Converter.cs
+++ NumbersToWords.Solution/NumbersToWords/Models/Converter.cs
@@ -1,21 +1,33 @@
 using System;
+using System.Collections.Generic;
 
 namespace NumbersToWords.Models
 {
   public class Converter
   {
-    public string Property1 { get; set; }
-    public int Property2 { get; set; }
+    public string Numeric { get; set; }
+    public Dictionary<string, string> Ones { get; set; }
+    public Dictionary<string, string> Teens { get; set; }
+    public Dictionary<string, string> Tens { get; set; }
 
-    public Converter(string property1, int property2)
+    public Converter(string numeric)
     {
-      Property1 = property1;
-      Property2 = property2;
+      Numeric = numeric;
+      Ones = new Dictionary<string, string>()
+      { { "1", "one" }, { "2", "two" }, { "3", "three" }, { "4", "four" }, { "5", "five" }, { "6", "six" }, { "7", "seven" }, { "8", "eight" }, { "9", "nine" }
+      };
+      Teens = new Dictionary<string, string>()
+      { { "10", "ten" }, { "11", "eleven" }, { "12", "twelve" }, { "13", "thirteen" }, { "14", "fourteen" }, { "15", "fifteen" }, { "16", "sixteen" }, { "17", "seventeen" }, { "18", "eighteen" }, { "19", "nineteen" },
+      };
+      Tens = new Dictionary<string, string>()
+      { { "2", "twenty" }, { "3", "thirty" }, { "4", "fourty" }, { "5", "fifty" }, { "6", "sixty" }, { "7", "seventy" }, { "8", "eighty" }, { "9", "ninety" },
+      };
     }
 
-    public bool MethodName()
+    public string ConvertToWords()
     {
-      return true;
+
+      return "hi";
     }
   }
 }
